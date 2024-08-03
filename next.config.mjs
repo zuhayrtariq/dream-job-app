da06c0f86c4f98e4a208a9741ba5f3ccf5087c16
Zuhayr Tariq <zuhayrtariq4@gmail.com>
2024-08-03 19:52:40 +0500

    Initial commit

---
+++ next.config.mjs
@@ -0,0 +1,13 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'zuhayr-job-board.s3.amazonaws.com',
+      },
+    ],
+  },
+};
+
+export default nextConfig;
---
+++ src/app/layout.tsx
@@ -0,0 +1,30 @@
+import Header from "@/app/components/Header";
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import "./globals.css";
+import '@radix-ui/themes/styles.css';
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: "Dream Jobs",
+  description: "Dream Jobs is a dynamic job portal built with Next.js, designed to connect job seekers with their ideal opportunities. With a sleek, responsive interface and lightning-fast performance, Dream Jobs makes job searching and recruitment easier than ever",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body className={inter.className}>
+        <Header />
+        {children}
+        <footer className="container py-8 text-gray-600">
+          Dream Job App &copy; 2024 - All rights reserved
+        </footer>
+      </body>
+    </html>
+  );
+}
